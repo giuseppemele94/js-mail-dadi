532fccc259b3784c9c55948d9f8e1bf37c02ac1e
Giuseppe Mele <giuseppemele9410@gmail.com>
2025-12-04 15:21:19 +0100

    feat : esercizio mail fatto

--- 01-mail/script.js
+++ 01-mail/script.js
@@ -6,3 +6,34 @@ Non è consentito usare nessun metodo proprio degli array (come includes, per es
 Si può fare? Certo che si basta ragionare un po’.
 Nota: 
 Non è necessario provvedere alla validazione delle email */
+
+
+//creo una lista di email 
+const guestList = [
+    "mario_rossi@gmail.com",
+    "anna_bianchi@gmail.com",
+    "luca_verdi@gmail.com",
+    "giulia_neri@gmail.com",
+    "giulia_rossi@gmail.com"
+];
+
+//chiedo all'utente di inserire una mail 
+const guestEmail = prompt("Inserisci la tua email :"); 
+//variabile per salvare il risultato 
+let found = false ; 
+
+
+//ciclo tutta la lista per trovare una corrispondenza 
+for(let i =0 ; i < guestList.length ; ++i) {
+
+    //se trovo una corrispondenza cambio la variabile in true e stampo email trovata
+    if(guestList[i] === guestEmail) {
+        found = true ; 
+        console.log("Email presente, puoi accedere !", guestEmail); 
+    }
+}
+
+//alrimenti stampo email non trovata
+ if(!found) {
+        console.log("Email non presente, non puoi accedere"); 
+    }
